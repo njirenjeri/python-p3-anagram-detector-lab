d77ef1aad77cedb35e5f8f7990accdb91d188cc9
Njire Njeri <njirenjeri@gmail.com>
2025-03-01 14:28:30 +0300

    anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, words):
+        return [myword for myword in words if sorted(myword.lower()) == sorted(self.word) and myword.lower() != self.word]
+        
